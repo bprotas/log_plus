c599f01d1718a19a91fe742152b8c0eb07df80b8
Brooke Kuhlmann <brooke@redalchemist.com>
2014-07-20 16:52:16 -0600

    Refactored specs (added contexts for local and remote environments).

--- spec/lib/log_plus/engine_spec.rb
+++ spec/lib/log_plus/engine_spec.rb
@@ -7,55 +7,84 @@ describe LogPlus::Engine do
 
   before { Rails.env = "test" }
 
-  context "default settings" do
-    it "sets log size to 1MB" do
-      expect(Rails.application.config.max_log_size).to eq(1)
-    end
+  describe "settings" do
+    context "common defaults" do
+      it "sets log size to 1MB" do
+        expect(Rails.application.config.max_log_size).to eq(1)
+      end
 
-    it "sets log tag datetime (first parameter)" do
-      travel_to Time.current do
-        now = Time.current
-        param = Rails.application.config.log_tags.first.call now
+      it "sets log tag datetime (first parameter)" do
+        travel_to Time.current do
+          now = Time.current
+          param = Rails.application.config.log_tags.first.call now
 
-        expect(param).to eq(now)
+          expect(param).to eq(now)
+        end
+      end
+
+      it "sets log tag datetime (second parameter)" do
+        param = Rails.application.config.log_tags.last
+        expect(param).to eq(:remote_ip)
       end
     end
 
-    it "sets log tag datetime (second parameter)" do
-      param = Rails.application.config.log_tags.last
-      expect(param).to eq(:remote_ip)
+    context "local environment (test or development)" do
+      it "does not configure the logger" do
+        skip "fails to pass when run with all specs (but doesn't when tested individually)" do
+          initializer.run LogPlus::Engine
+          logger = Rails.application.config.logger
+
+          expect(logger).to eq(nil)
+        end
+      end
     end
 
-    it "set logger to keep 7 logs max at 5MB each" do
-      Rails.env = "production"
-      initializer.run Rails.application
+    context "non-local environment (not test or development)" do
+      before { Rails.env = "production" }
 
-      logger = Rails.application.config.logger
+      it "set logger to keep 7 logs max at 5MB each" do
+        initializer.run Rails.application
 
-      expect(logger.inspect).to match(/shift_age=7/)
-      expect(logger.inspect).to match(/shift_size=5248000/)
+        logger = Rails.application.config.logger
+
+        expect(logger.inspect).to match(/shift_age=7/)
+        expect(logger.inspect).to match(/shift_size=5248000/)
+      end
     end
   end
 
-  context "log cleaning" do
+  describe "log cleaning" do
     let(:small_log) { File.join Dir.pwd, "spec", "support", "small.log.example" }
     let(:large_log) { File.join Dir.pwd, "spec", "support", "large.log.example" }
     let(:test_log) { File.join Dir.pwd, "spec", "dummy", "log", "test.log" }
 
-    it "clears logs larger than max size" do
-      FileUtils.cp large_log, test_log
-      initializer.run Rails.application
+    context "local environment (test or development)" do
+      it "clears logs larger than max size" do
+        FileUtils.cp large_log, test_log
+        initializer.run Rails.application
 
-      expect(File.size(test_log)).to eq(0)
+        expect(File.size(test_log)).to eq(0)
+      end
+
+      it "does not clear logs smaller than max size" do
+        FileUtils.cp small_log, test_log
+        initializer.run Rails.application
+        first_line = File.open(test_log, &:readline)
+
+        expect(File.size(test_log) > 0).to eq(true)
+        expect(first_line).to match(/This is a small log/)
+      end
     end
 
-    it "does not clear logs smaller than max size" do
-      FileUtils.cp small_log, test_log
-      initializer.run Rails.application
-      first_line = File.open(test_log, &:readline)
+    context "non-local environment (not test or development)" do
+      before { Rails.env = "production" }
 
-      expect(File.size(test_log) > 0).to eq(true)
-      expect(first_line).to match(/This is a small log/)
+      it "does not clear logs larger than max size" do
+        FileUtils.cp large_log, test_log
+        initializer.run Rails.application
+
+        expect(File.size(test_log) > 0).to eq(true)
+      end
     end
   end
 end
